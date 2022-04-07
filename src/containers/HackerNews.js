4daaa68e8397928b327a5b8692e24d0cbd660caa
Szymon <wojdyla.szymon@gmail.com>
2022-04-07 12:20:28 +0100

    getting individual story object return and added to state of TopStories

--- src/containers/HackerNews.js
+++ src/containers/HackerNews.js
@@ -1,7 +1,49 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import TopStories from '../components/TopStories';
 
 const HackerNews = () => {
+
+    const [topStories, setTopStories] = useState([]);
+
+    useEffect(() => {
+        getTopStories();
+        
+    }, [])
+
+    const getTopStories = () => {
+        fetch('https://hacker-news.firebaseio.com/v0/topstories.json')
+        .then(response => response.json())
+        // .then(data => console.log(data))
+        .then((data) => {data.map((storyId)=> {
+            return (
+                fetch(`https://hacker-news.firebaseio.com/v0/item/${storyId}.json`)
+                .then(response => response.json())
+                
+                // .then(data => console.log(data))
+            )
+        .then(stories => setTopStories(stories))
+        .then(console.log(topStories))
+            
+        })});
+    }
+    // const getTopStoryIds = () => {
+    //     fetch('https://hacker-news.firebaseio.com/v0/topstories.json')
+    //     .then(response => response.json())
+        
+    //     .then(data => {return data})
+        
+    // }
+
+    
+    
+    
+    
+    // const getTopStories = () => {
+        
+    //     const storyIds = getTopStoryIds();
+    //     console.log(storyIds);
+
+    // }
 
 
     return (
